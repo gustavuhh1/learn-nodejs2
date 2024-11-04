4fb9a94cabe2e92d9b5300b5f8bf34b46ea999de
GustavoMartinsR <gustavomrodrigues11@gmail.com>
2024-11-04 13:59:32 -0300

    fix: DatabaseURL

--- src/database.ts
+++ src/database.ts
@@ -7,20 +7,18 @@ if(!process.env.DATABASE_URL){
 
 
 export const config: Knex.Config = {
-    client: env.DATABASE_CLIENT,
-    connection: 
-        env.DATABASE_CLIENT === 'sqlite' 
-        ? {
-            filename: env.DATABASE_CLIENT
-            }    
-        : env.DATABASE_CLIENT,
-    useNullAsDefault: true,
-    migrations: {
-        extension: 'ts',
-        directory: './db/migrations',
-    }
-    
-    
-}
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+};
 
 export const knex = setupKnex(config)
